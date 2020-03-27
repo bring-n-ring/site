43530ee365e77b4d3ce53550cb46c3f1664deaa5
Marco Karaula <marco.karaula@flixbus.com>
2020-03-27 21:55:01 +0100

    Section 4 layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react'
 import Page from '../components/Page'
 import IndexLayout from '../layouts'
-import { Entry, Insta } from '../sections'
+import { Entry, Insta, OurMission } from '../sections'
 import entryData from '../content/home/sections/entry.yaml'
 import instaData from '../content/home/sections/insta.yaml'
+import ourMission from '../content/home/sections/our-mission.yaml'
 
 const IndexPage = () => (
   <IndexLayout>
     <Page>
+      <OurMission {...ourMission} />
       <Entry {...entryData} />
       <Insta {...instaData} />
     </Page>
