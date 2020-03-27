25517934b34a4856ec102dce074039e125af978f
jonathan.lechner <16844326+hinterdupfinger@users.noreply.github.com>
2020-03-28 00:13:26 +0100

    adding content (styling still missing and slider sync and interaction)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,18 @@
 import * as React from 'react'
 import Page from '../components/Page'
-import IndexLayout from '../layouts'
-import { Entry, Insta, OurMission } from '../sections'
+import appDescriptionData from '../content/home/sections/app-description.yaml'
 import entryData from '../content/home/sections/entry.yaml'
 import instaData from '../content/home/sections/insta.yaml'
 import ourMission from '../content/home/sections/our-mission.yaml'
+import IndexLayout from '../layouts'
+import { Entry, Insta, OurMission } from '../sections'
+import AppDescription from '../sections/app-description/AppDescription'
 
 const IndexPage = () => (
   <IndexLayout>
     <Page>
       <Entry {...entryData} />
+      <AppDescription {...appDescriptionData} />
       <Insta {...instaData} />
       <OurMission {...ourMission} />
     </Page>
---
+++ src/sections/app-description/AppDescription.tsx
@@ -0,0 +1,73 @@
+import { Typography } from '@bring-n-ring/components'
+import { IonButton, IonContent, IonSlide, IonSlides } from '@ionic/react'
+import * as React from 'react'
+import Phone from '../../components/phone/Phone'
+import styles from './app-description.module.css'
+
+const slideOpts = {
+  initialSlide: 1,
+  speed: 400
+}
+
+export type AppDescriptionProps = {
+  title: string
+  body: string
+  btnText: string
+  slides: [
+    {
+      src: string
+      alt: string
+      title: string
+      description: string
+    }
+  ]
+}
+
+const AppDescription: React.FC<AppDescriptionProps> = ({ title, body, btnText, slides }) => {
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.content}>
+        <Typography className={styles.title} color="primary" size="h1">
+          {title}
+        </Typography>
+        <Typography className={styles.body} size="p">
+          {body}
+        </Typography>
+        <div className={styles.alignCenter}>
+          <IonButton>{btnText}</IonButton>
+        </div>
+      </div>
+      <div className={styles.width}>
+        <Phone>
+          <IonContent className={styles.img}>
+            <IonSlides options={slideOpts} className={styles.img}>
+              {slides.map(slide => (
+                <IonSlide>
+                  <div className={styles.img}>
+                    <img src={slide.src} alt={slide.alt} className={styles.img} />
+                  </div>
+                </IonSlide>
+              ))}
+            </IonSlides>
+          </IonContent>
+        </Phone>
+      </div>
+      <div className={styles.description}>
+        <IonSlides options={slideOpts}>
+          {slides.map(slide => (
+            <IonSlide>
+              <Typography className={styles.title} size="h2">
+                {slide.title}
+              </Typography>
+              <Typography className={styles.body} size="p">
+                {slide.description}
+              </Typography>
+            </IonSlide>
+          ))}
+        </IonSlides>
+      </div>
+    </div>
+  )
+}
+
+export default AppDescription
